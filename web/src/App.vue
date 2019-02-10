47652e407841a563e8f7f9742ef743194ff3e010
Yuan <meseta@gmail.com>
2019-02-10 15:34:17 +0000

    removed boilerplate

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,23 +1,33 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar flat app>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <router-link to="/" tag="span" style="cursor: pointer" class="font-weight-light">
+          FireBin
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat class="grey--text">
+          <v-icon left>save</v-icon>Save
+        </v-btn>
+        <v-btn flat class="grey--text">
+          <v-icon left>add_box</v-icon>New
+        </v-btn>
+        <v-btn flat class="grey--text">
+          <v-icon left>file_copy</v-icon>Copy
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App'
+
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- web/src/main.js
+++ web/src/main.js
@@ -5,9 +5,9 @@ import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
-
 Vue.use(Vuetify)
+
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
