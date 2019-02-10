683c7a5c3440acfb0f68b96cd55ff8e639637561
Yuan <meseta@gmail.com>
2019-02-10 15:47:46 +0000

    add vuex

--- web/src/main.js
+++ web/src/main.js
@@ -1,13 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import './plugins/vuetify'
-import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
 
-Vue.use(Vuetify)
-
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
---
+++ web/src/store/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+// import router from '@/router'
+// import * as firebase from 'firebase/app'
+// import 'firebase/auth'
+// import 'firebase/firestore'
+
+Vue.use(Vuex)
+
+const state = {
+
+}
+
+const getters = {
+
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions,
+  strict: process.env.NODE_ENV !== 'production'
+})
