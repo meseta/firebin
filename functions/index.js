d1340923582ae7aa672d960b9af3339680b9bdd6
Yuan <meseta@gmail.com>
2019-02-15 02:37:26 +0000

    added bot endpoint, and start of editing

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const pako = require('pako');
 const admin = require('firebase-admin');
+const firebase = require('firebase');
 admin.initializeApp();
 
 const runtimeOpts = {
@@ -31,12 +32,49 @@ function returnOk(res, msg) {
   return
 }
 
+exports.editFirebin = functions
+  .runWith(runtimeOpts)
+  .https.onCall((body, context) => {
+
+    let binId = body.binId
+    let editId = body.editId
+
+
+    return admin.firestore().collection('firebin-edit').doc(editId).get()
+    .then(doc => {
+      if (doc.get('binId') !== binId) {
+        throw new StatusException(403, 'Access Denied')
+      }
+      let edits = doc.get('edits') || 0;
+      edts += 1;
+
+      return admin.firestore().collection('firebin-edit').doc(editId).set({
+        edits: edits
+      }, {merge: true})
+
+    })
+    .then(() => {
+
+      return admin.firestore().collection('firebin').doc(binId).set({
+        data: body.data,
+        encode: body.encode,
+        compress: body.compress
+      }, {merge: true})
+    })
+    .then(() => {
+      return {binId: binId, editId: editId}
+    })
+  });
+
+
 exports.saveFirebin = functions
   .runWith(runtimeOpts)
   .https.onCall((body, context) => {
 
     let ip = context.rawRequest.headers['x-forwarded-for']
 
+    let binId
+    let editId
     return admin.firestore().collection('firebin').add({
       ip: ip,
       created: admin.firestore.FieldValue.serverTimestamp(),
@@ -45,27 +83,66 @@ exports.saveFirebin = functions
       compress: body.compress
     })
     .then(doc => {
-      return {binId: doc.id}
+      binId = doc.id
+
+      return admin.firestore().collection('firebin-edit').add({
+        edits: 0,
+        created: admin.firestore.FieldValue.serverTimestamp(),
+        binId: binId
+      })
+    })
+    .then(doc => {
+      editId = doc.id
+      return {binId: binId, editId: editId}
     })
   });
-/*
-exports.loadFirebin = functions
+
+exports.saveFirebinExt = functions
   .runWith(runtimeOpts)
   .https.onRequest((req, res) => {
 
-    return admin.firestore().collection('firebin').doc(req.binId).get()
-    .then(doc => {
-      if (!doc.exists) {
-        throw new StatusException(404, "firebin not found")
-      }
+    let ip = req.headers['x-forwarded-for']
+    console.log(req.headers)
+    let data = req.body
+    let encode = 'text'
+    let compress = 'none'
+    console.log(data)
+    let input = Buffer.from(data, 'utf-8')
+    let b64str
 
-      return returnOk(res, {
-        data: doc.get('data'),
-        compress: doc.get('compress')
-        encode: doc.get('encode')
+    try {
+      let compress = pako.deflate(input)
+      let blob = firebase.firestore.Blob.fromUint8Array(compress)
+      b64str = blob.toBase64()
+    } catch (err) {
+      console.log(err)
+      throw new StatusException(500, 'Server Error')
+    }
+
+    if (b64str.length < data.length) {
+      data = b64str
+      encode = 'base64'
+      compress = 'zlib'
+    }
+
+    return admin.firestore().collection('firebin').add({
+      ip: ip,
+      created: admin.firestore.FieldValue.serverTimestamp(),
+      data: data,
+      encode: encode,
+      compress: compress
+    })
+    .then(doc => {
+      binId = doc.id
+
+      return admin.firestore().collection('firebin-edit').add({
+        edits: 0,
+        created: admin.firestore.FieldValue.serverTimestamp(),
+        binId: binId
       })
     })
-    .catch(err => {
-      return returnErr(res, err)
+    .then(doc => {
+      editId = doc.id
+      return returnOk(res, {binId: binId, editId: editId})
     })
-  }); */
+  });
